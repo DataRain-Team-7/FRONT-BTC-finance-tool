81d8dd61de08173dab91342d1c3cc55d0481431f
João Pedro Thuler Lima <thuler_lima@hotmail.com>
2022-12-06 17:36:33 -0300

    fix: changed component on login page to avoid possible conflict

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,12 +1,11 @@
 import * as Style from './style'
 import TopBar from '../../components/TopBar'
-import Header from '../../components/Header'
 
 
 const Login = () => {
     return (
         <Style.LoginContainer>
-            <Header />
+            <TopBar />
             <p>Login</p>
         
         </Style.LoginContainer>
