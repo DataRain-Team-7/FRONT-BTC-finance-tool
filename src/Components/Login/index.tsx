d88c3c6033674a5a0cf13a26765664c9ae138436
maxxcleiton <maxcleiton@live.com>
2022-12-06 23:25:16 -0300

    feat. div added, useStates and conditional rendering - Login

--- src/Components/Login/index.tsx
+++ src/Components/Login/index.tsx
@@ -1,54 +1,62 @@
+import { useState } from "react";
 import Ou from "./Ou";
 import * as Style from "./style";
 
 const Login = () => {
+  const [showRegisterForm, setShowRegisterForm] = useState(false);
+  const [showLoginForm, setShowLoginForm] = useState(true);
+
   return (
-    <Style.AuthFormContainer>
-      <Style.AuthFormContainer>
-        <Style.AuthForm>
-          <Style.AuthFormContent>
-            <Style.AuthFormTitle>Login</Style.AuthFormTitle>
-            <div>
-              <Style.Label>Email</Style.Label>
-              <input
-                type="email"
-                // className="form-control mt-1"
-                placeholder="Digite seu email"
-              />
-            </div>
-            <div>
-              <Style.Label>Senha</Style.Label>
-              <input
-                type="password"
-                // className="form-control mt-1"
-                placeholder="Digite sua senha"
-              />
-            </div>
-            <p>
-              <Style.LinkTo href="#">Esqueceu a senha?</Style.LinkTo>
-            </p>
+    <div>
+      {showLoginForm ? (
+        <Style.AuthFormContainer>
+          <Style.AuthFormContainer>
+            <Style.AuthForm>
+              <Style.AuthFormContent>
+                <Style.AuthFormTitle>Login</Style.AuthFormTitle>
+                <div>
+                  <Style.Label>Email</Style.Label>
+                  <input
+                    type="email"
+                    // className="form-control mt-1"
+                    placeholder="Digite seu email"
+                  />
+                </div>
+                <div>
+                  <Style.Label>Senha</Style.Label>
+                  <input
+                    type="password"
+                    // className="form-control mt-1"
+                    placeholder="Digite sua senha"
+                  />
+                </div>
+                <p>
+                  <Style.LinkTo href="#">Esqueceu a senha?</Style.LinkTo>
+                </p>
 
-            <Style.LembrarMe>Lembrar-me</Style.LembrarMe>
-            <input type={"checkbox"}></input>
+                <Style.LembrarMe>Lembrar-me</Style.LembrarMe>
+                <input type={"checkbox"}></input>
 
-            <div className="d-grid gap-2 mt-3">
-              <button
-                type="submit"
-                // className="btn btn-primary"
-              >
-                Entrar
-              </button>
-              <Ou />
-              <p>
-                <Style.LinkTo href="#">
-                  Clique aqui para criar uma nova conta
-                </Style.LinkTo>
-              </p>
-            </div>
-          </Style.AuthFormContent>
-        </Style.AuthForm>
-      </Style.AuthFormContainer>
-    </Style.AuthFormContainer>
+                <div className="d-grid gap-2 mt-3">
+                  <button
+                    type="submit"
+                    // className="btn btn-primary"
+                  >
+                    Entrar
+                  </button>
+                  <Ou />
+                  <p>
+                    <Style.LinkTo href="#">
+                      Clique aqui para criar uma nova conta
+                    </Style.LinkTo>
+                  </p>
+                </div>
+              </Style.AuthFormContent>
+            </Style.AuthForm>
+          </Style.AuthFormContainer>
+        </Style.AuthFormContainer>
+      ) : null}
+    </div>
   );
 };
 
