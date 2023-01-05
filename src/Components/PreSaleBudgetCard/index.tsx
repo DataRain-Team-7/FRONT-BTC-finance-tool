545a0e52fe0b8501acd68935bf3be05fd733fb7c
João Pedro Thuler Lima <thuler_lima@hotmail.com>
2023-01-05 11:17:02 -0300

    feat: scrollbar on presale page implemented

--- src/Components/PreSaleBudgetCard/index.tsx
+++ src/Components/PreSaleBudgetCard/index.tsx
@@ -14,9 +14,13 @@ const PreSaleBudgetCard = () =>{
                     <h3>Jenny Wilson</h3>
                 </div>
             </section>
+            <div className="title">
+                <h2 className="fisrth2">Questões</h2>
+                <h2>Horas</h2>
+                <h2>Valor/hr</h2>
+            </div>
             <section className="summary">
                 <div className="questions">
-                    <h2>Questões</h2>
                     <div>
                         <h4>1- Quem veio primeiro, o ovo ou a galinha?</h4>
                         <p>* O ovo veio primeiro</p>
@@ -31,29 +35,41 @@ const PreSaleBudgetCard = () =>{
                         <h4>3- Quem veio primeiro, o ovo ou a galinha?</h4>
                         <p>* O ovo veio primeiro</p>
                         <p>* Porém era de codorna</p>
+                    </div>
+                    <div>
+                        <h4>4- Quem veio primeiro, o ovo ou a galinha?</h4>
+                        <p>* O ovo veio primeiro</p>
+                        <p>* Porém era de codorna</p>
+                    </div>
+                    <div>
+                        <h4>5- Quem veio primeiro, o ovo ou a galinha?</h4>
+                        <p>* O ovo veio primeiro</p>
+                        <p>* Porém era de codorna</p>
                     </div>    
                 </div>
                 <div className="hours">
-                    <h2>Horas</h2>
                     <div>
                         <p>50hr</p>
+                        <p>72hr</p>
+                        <p>72hr</p>
                         <p>72hr</p>
                         <p>72hr</p>
                     </div>
                 </div>
                 <div className="value">
-                    <h2>Valor/hr</h2>
                     <div>
                         <p>R$: 159,90</p>
                         <p>R$: 140,90</p>
+                        <p>R$: 199,90</p>
+                        <p>R$: 199,90</p>
                         <p>R$: 199,90</p>
                     </div>
                 </div>
             </section>
             <section>
                 <h2>Nota sobre o orçamento</h2>
-                <input></input>
-                <div>
+                <textarea wrap="hard" placeholder="Comentário adicional"></textarea>
+                <div className="extract">
                     <p>Horas Totais: 194hr</p>
                     <p>Valor Total - R$ 39.509,50</p>
                 </div>
--- src/Components/PreSaleBudgetCard/style.ts
+++ src/Components/PreSaleBudgetCard/style.ts
@@ -19,21 +19,87 @@ export const PreSaleBudgetContainer = styled.div`
         justify-content: space-around;
     }
 
+    .title{
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        padding: 0 1rem;
+        h2{
+            width: 25%;
+        }
+        .fisrth2{
+            width: 50%;
+        }
+        
+        
+    }
+
     .summary{
         display: flex;
         justify-content: space-between;
+        height: 50%;
+        overflow-y: scroll;
+            ::-webkit-scrollbar {
+                width: 10px;
+            }
+            ::-webkit-scrollbar-track {
+                background: ${theme.colors.inputFontColor};
+                border-radius:5px;
+            }
+            ::-webkit-scrollbar-thumb {
+                background: ${theme.colors.tertiaryColor};
+                border-radius:5px;
+            }
+            /* ::-webkit-scrollbar-thumb:hover {
+                background: #555;
+            } */
+            
+
 
         .questions{
             width: 50%;
+            
+            div{
+                margin-left: 1rem;
+                height: 10vh;
+                
+                p{
+                    margin: .5rem 0;
+                    margin-left: 1rem;
+                }
+            }
         }
 
-        .hours{
+        .hours, .value{
             width: 25%;
+
+            h2{
+                margin-bottom: 3vh;
+            }
+            p{
+                margin-left: 1rem;
+                height: 10vh;  
+            }
         }
 
-        .value{
-            width: 25%;
-        }
+        
     }
+
+    textarea{
+                    margin-left: 1px;
+                    resize: none;
+                    width: 100%;
+                    height: 9vh;
+                    padding: 5px;
+                    background-color: #f2f4f6;
+                    border: none;
+                }
+
+                .extract{
+            display: flex;
+            flex-direction: row;
+            width: 100%;
+            justify-content: space-evenly;
+        }
     
   `}`;
