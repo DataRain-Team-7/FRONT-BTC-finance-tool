6882a9401d860469a1756099ea1f6c419bd01a1c
João Pedro Thuler Lima <thuler_lima@hotmail.com>
2023-01-18 23:05:41 -0300

    feat: logic to save answers on form page

--- src/Components/FormCard/SecondPage/index.tsx
+++ src/Components/FormCard/SecondPage/index.tsx
@@ -11,25 +11,41 @@ interface FirstPageProp {
 }
 
 const SecondPageCard = ({setStepNumber}:FirstPageProp) =>{
-
+    
     const idClient = sessionStorage.getItem("clientId")
     const [ count, setCount] = useState<number>(0)
-    const [ answers, setAnswers ] =useState<any>()
+    const [ answers, setAnswers ] =useState<any>([])
     const [ value, setValue ] = useState<string>()
-    const [ text, setText ] = useState<string>()
-
+    const [ oldValue, setOldValue ] = useState<string>()
+    const [ text, setText ] = useState<string>('')
+    const [ isModalOpen, setIsModalOpen ] = useState<boolean>(false)
+    const { questions } = useQuestions()
+    
     const data = {
         clientId: idClient,
         responses: answers
     }
 
-    // [
+    // let teste2 = [
     //     {
-    //     "questionId": "ac06f36e-4b61-4fe8-8fd6-6ad807ac6282",
-    //     "alternativeId": "ac06f36e-4b61-4fe8-8fd6-6ad807ac6282",
-    //     "responseDetails": "Preciso que tenham estes detalhes..."
+    //     questionId: questions[count].id,
+    //     alternativeId: value,
+    //     responseDetails: text,
     //     }
     // ]
+
+    const handleCurrentQuestion = () =>{
+        let content = answers
+        content[count] = {
+            questionId: questions[count].id,
+            alternativeId: value,
+            responseDetails: text,
+        }
+        setAnswers(content)
+        setValue(undefined)
+        setText("")
+        console.log(answers)
+    }
 
     const handleBack = () =>{
         if(count === 0){
@@ -37,34 +53,34 @@ const SecondPageCard = ({setStepNumber}:FirstPageProp) =>{
         }else{
             setCount(count - 1)
         }
+        setValue(oldValue)
     }
 
     const handleNext = () =>{
+        if(value !== undefined)setOldValue(value)      
         if(count === questions.length-1){
             setIsModalOpen(true)
             //aqui o questionario foi preenchido
         }else{
+            handleCurrentQuestion()
             setCount(count + 1)
+            console.log(answers)
             //aqui deve ser adicionado cada resposta nova no objeto
         }
     }
-
-    const [ isModalOpen, setIsModalOpen ] = useState<boolean>(false)
-    const { questions } = useQuestions()
-     
 
     return(
         <Style.SecondPageCard>
             <section >
                     <h3>{`${count+1}- ${questions[count].description}`}</h3>
                 <FormControl className="FormControl">
-                    <RadioGroup value={value} onChange={(e)=>setValue(e.target.value)}>
+                    <RadioGroup defaultValue={value} onChange={(e)=>setValue(e.target.value)}>
                         {questions[count].alternatives.map((e:any)=>{                    
-                            return <FormControlLabel value={e.id} control={<Radio />} label={e.description}/>
+                            return <FormControlLabel key={e.id} value={e.id} control={<Radio />} label={e.description}/>
                         })}
                     </RadioGroup>                   
                 </FormControl>
-                <textarea onChange={(e)=>setText(e.target.value)} wrap="hard" placeholder="Comentário adicional"></textarea>
+                <textarea value={text} onChange={(e)=>setText(e.target.value)} wrap="hard" placeholder="Comentário adicional"></textarea>
                 {/* <div onClick={()=>console.log(questions[count].alternatives.map((e:any)=>{return e.description}))}>Botaão louco</div> */}
             </section>
             <div className="pageButtons">
