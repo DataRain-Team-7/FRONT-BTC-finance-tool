62b6f7975c199b980af1cfc4e33e54403b72ffbf
João Pedro Thuler Lima <thuler_lima@hotmail.com>
2023-02-02 19:12:16 -0300

    feat: logic to aprove and reprove extrahour

--- src/components/ExtraHourCard/index.tsx
+++ src/components/ExtraHourCard/index.tsx
@@ -2,12 +2,48 @@ import { useNavigate } from "react-router-dom";
 import * as Style from "./style"
 import { useUsers } from "../../contexts/userContext";
 import { useAuth } from "../../contexts/auth";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@mui/material";
-
-// useEffect(()=> {},[])
+import Api from "../../services/api";
+import { toast } from "react-hot-toast";
 
 const ExtraHourCard = () => {
+
+    useEffect(()=> handleGetRequest(),[])
+
+    const [ requests, setRequests ] = useState<any>()
+
+    const handleGetRequest = () => {
+      Api.get(`/request-send-overtime`)
+        .then((res)=>setRequests(res.data))
+        .catch(()=>{})
+    }
+
+    const handleAprove = (id: string) =>{
+      Api.post(`/request-send-overtime/approve`,
+      {
+        requestSendOvertimeId: id
+      }
+      )
+      .then(()=>{
+        handleGetRequest();
+        toast.success("Feito")
+      })
+      .catch(()=>toast.error("Não foi possível concluir"))
+    }
+
+    const handleDesaprove = (id: string) =>{
+      Api.post(`/request-send-overtime/reprove`,
+      {
+        requestSendOvertimeId: id
+      }
+      )
+      .then(()=>{
+        handleGetRequest();
+        toast.success("Feito")
+      })
+      .catch(()=>toast.error("Não foi possível concluir"))
+    }
   
   return (
           <Style.ExtraHourContainer>
@@ -15,25 +51,8 @@ const ExtraHourCard = () => {
                 <h2>Pedidos de hora extra</h2>            
               </section>
               <section className="section02">
-              <section>
-                  <div>
-                    <h2>Colaborador</h2>
-                    <h2>Projeto</h2>
-                    <h2>Cliente</h2>
-                  </div>
-                  <div>
-                    <p>Joao Pedro</p>
-                    <p>Projeto 01</p>
-                    <p>Joao corporation</p>
-                  </div>
-                  <div>
-                    <p className="explanation">Comentário de requisiçao de hora extra</p>
-                  </div>
-                  <div className="botton">
-                    <Button  variant="contained" className="buttonEnter" onClick={()=>{}}>Aprovar pedido</Button>
-                    <Button  variant="contained" className="buttonEnter reprove" onClick={()=>{}}>Negar pedido</Button>
-                  </div>
-                </section>
+                {requests && requests.map((element:any)=>{
+                return( 
                 <section>
                   <div>
                     <h2>Colaborador</h2>
@@ -41,37 +60,19 @@ const ExtraHourCard = () => {
                     <h2>Cliente</h2>
                   </div>
                   <div>
-                    <p>Joao Pedro</p>
-                    <p>Projeto 01</p>
-                    <p>Joao corporation</p>
+                    <p>{element.user.name}</p>
+                    <p>{element.project.name}</p>
+                    <p>{element.client.companyName}</p>
                   </div>
                   <div>
-                    <p className="explanation">Comentário de requisiçao de hora extra</p>
+                    <p className="explanation">{element.requestDescription}</p>
                   </div>
                   <div className="botton">
-                    <Button  variant="contained" className="buttonEnter" onClick={()=>{}}>Aprovar pedido</Button>
-                    <Button  variant="contained" className="buttonEnter reprove" onClick={()=>{}}>Negar pedido</Button>
+                    <Button  variant="contained" className="buttonEnter" onClick={()=>handleAprove(element.requestSendOvertimeId)}>Aprovar pedido</Button>
+                    <Button  variant="contained" className="buttonEnter reprove" onClick={()=>handleDesaprove(element.requestSendOvertimeId)}>Negar pedido</Button>
                   </div>
                 </section>
-                <section>
-                  <div>
-                    <h2>Colaborador</h2>
-                    <h2>Projeto</h2>
-                    <h2>Cliente</h2>
-                  </div>
-                  <div>
-                    <p>Joao Pedro</p>
-                    <p>Projeto 01</p>
-                    <p>Joao corporation</p>
-                  </div>
-                  <div>
-                    <p className="explanation">Comentário de requisiçao de hora extra</p>
-                  </div>
-                  <div className="botton">
-                    <Button  variant="contained" className="buttonEnter" onClick={()=>{}}>Aprovar pedido</Button>
-                    <Button  variant="contained" className="buttonEnter reprove" onClick={()=>{}}>Negar pedido</Button>
-                  </div>
-                </section>
+                )})}
               </section>
           </Style.ExtraHourContainer>  
   );
